f93405120af963289b8e56e9b5ba3e7a5bb816d3
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-10-19 21:03:47 +0200

    ...

--- menu.sh
+++ menu.sh
@@ -9,7 +9,7 @@ source /etc/functions.sh
 RESULT=2
 
 clear;
-echo 'wireguard=true'
+echo 'wireguard='true'
 ' | sudo -E tee $HOME/yiimpserver/yiimp_single/.wireguard.install.cnf >/dev/null 2>&1;
 echo 'server_type='db'
 DBInternalIP='10.0.0.2'
