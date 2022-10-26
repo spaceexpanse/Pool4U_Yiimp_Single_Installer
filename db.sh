ffc5db3ed7ae45251cd745fe295eee76911301cb
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-10-26 22:36:48 +0200

    ...

--- db.sh
+++ db.sh
@@ -32,9 +32,9 @@ trap print_error ERR
 
 echo -e "$CYAN Installing MariaDB 10.4...$COL_RESET"
 MARIADB_VERSION='10.4'
-hide_output sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password password $DBRootPassword"
-hide_output sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password_again password $DBRootPassword"
-hide_output apt_install mariadb-server mariadb-client
+sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password password $DBRootPassword"
+sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password_again password $DBRootPassword"
+apt_install mariadb-server mariadb-client
 echo -e "$GREEN MariaDB build complete...$COL_RESET"
 echo -e " Creating DB users for YiiMP...$COL_RESET"
 
@@ -43,7 +43,7 @@ Q2="GRANT ALL ON ${YiiMPDBName}.* TO '${YiiMPPanelName}'@'${Host_Napravi}' IDENT
 Q3="GRANT ALL ON ${YiiMPDBName}.* TO '${StratumDBUser}'@'${Host_Napravi}' IDENTIFIED BY '$StratumUserDBPassword';"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
-hide_output sudo mysql -u root -p"${DBRootPassword}" -e "$SQL"
+sudo mysql -u root -p"${DBRootPassword}" -e "$SQL"
 
 echo -e "$GREEN Database creation complete...$COL_RESET"
 
@@ -66,29 +66,29 @@ user=root
 password='"${DBRootPassword}"'
 ' | sudo -E tee $STORAGE_ROOT/yiimp/.my.cnf >/dev/null 2>&1
 
-hide_output sudo chmod 0600 $STORAGE_ROOT/yiimp/.my.cnf
+sudo chmod 0600 $STORAGE_ROOT/yiimp/.my.cnf
 echo -e "$GREEN Passwords can be found in $STORAGE_ROOT/yiimp/.my.cnf$COL_RESET"
 
 echo -e "$CYAN Importing YiiMP Default database values...$COL_RESET"
-hide_output cd $HOME/yiimp/yiimp_setup/yiimp/sql
+cd $HOME/yiimp/yiimp_setup/yiimp/sql
 # import sql dump
-hide_output sudo zcat 2019-11-10-yiimp.sql.gz | sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}"
-hide_output sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2018-09-22-workers.sql
-hide_output sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2020-06-03-blocks.sql
+sudo zcat 2019-11-10-yiimp.sql.gz | sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}"
+sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2018-09-22-workers.sql
+sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2020-06-03-blocks.sql
 echo -e "$GREEN Database import complete...$COL_RESET"
 
 echo -e "$CYAN Tweaking MariaDB for better performance...$COL_RESET"
-
-hide_output sudo sed -i '/max_connections/c\max_connections         = 2000' /etc/mysql/my.cnf
-hide_output sudo sed -i '/thread_cache_size/c\thread_cache_size       = 512' /etc/mysql/my.cnf
-hide_output sudo sed -i '/tmp_table_size/c\tmp_table_size          = 128M' /etc/mysql/my.cnf
-hide_output sudo sed -i '/max_heap_table_size/c\max_heap_table_size     = 128M' /etc/mysql/my.cnf
-hide_output sudo sed -i '/wait_timeout/c\wait_timeout            = 60' /etc/mysql/my.cnf
-hide_output sudo sed -i '/max_allowed_packet/c\max_allowed_packet      = 64M' /etc/mysql/my.cnf
-hide_output sudo sed -i 's/#bind-address=0.0.0.0/bind-address='${DBInternalIP}'/g' /etc/mysql/my.cnf
-
+sudo sed -i '/max_connections/c\max_connections         = 2000' /etc/mysql/my.cnf
+sudo sed -i '/thread_cache_size/c\thread_cache_size       = 512' /etc/mysql/my.cnf
+sudo sed -i '/tmp_table_size/c\tmp_table_size          = 128M' /etc/mysql/my.cnf
+sudo sed -i '/max_heap_table_size/c\max_heap_table_size     = 128M' /etc/mysql/my.cnf
+sudo sed -i '/wait_timeout/c\wait_timeout            = 60' /etc/mysql/my.cnf
+sudo sed -i '/max_allowed_packet/c\max_allowed_packet      = 64M' /etc/mysql/my.cnf
+sudo sed -i 's/#bind-address=0.0.0.0/bind-address='${DBInternalIP}'/g' /etc/mysql/my.cnf
 echo -e "$GREEN Database tweak complete...$COL_RESET"
-hide_output restart_service mysql
+
+restart_service mysql
 echo -e "$GREEN Database build complete...$COL_RESET"
+
 set +eu +o pipefail
 cd $HOME/yiimpserver/yiimp_single
