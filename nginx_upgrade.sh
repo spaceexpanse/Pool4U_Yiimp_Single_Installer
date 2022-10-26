5d37a645923cb23f7c453ee41da40ce60bd822f4
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-10-27 01:43:56 +0200

    ...

--- nginx_upgrade.sh
+++ nginx_upgrade.sh
@@ -29,8 +29,7 @@ echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list >/dev/null 2>&1
 
 sudo curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add - >/dev/null 2>&1
-sudo apt-get update
-sudo apt-get nginx
+sudo apt-get nginx -y
 
 # Make additional conf directories, move and generate needed configurations.
 sudo mkdir -p /etc/nginx/yiimp.server
